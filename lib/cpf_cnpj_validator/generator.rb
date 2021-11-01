9c744766667ad99aefc0d692ed859e6678c496ac
Emerson Freitas <mersonsi@gmail.com>
2021-11-01 19:45:44 -0300

    feat(cnpj): generator

---
+++ lib/cpf_cnpj_validator/generator.rb
@@ -0,0 +1,12 @@
+module CpfCnpjValidator
+  class Generator
+    NUMBERS = Array(0..9).freeze
+
+    def self.generate(size, verifier_digit_generator)
+      numbers = Array.new(size) { NUMBERS.sample }
+      numbers << verifier_digit_generator.generate(numbers)
+      numbers << verifier_digit_generator.generate(numbers)
+      numbers.join("")
+    end
+  end
+end
